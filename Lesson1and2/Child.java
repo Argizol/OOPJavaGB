7f2db7156726ec7af6c26fb4d11af70ecaf62cd8
Dmitriy <argizol@mail.ru>
2022-11-13 21:20:36 +0300

    Lesson 3 решение update

--- Lesson1and2/Child.java
+++ Lesson1and2/Child.java
@@ -10,16 +10,20 @@ public class Child extends Human {
 
 	static List<Child> childs = new ArrayList<>();
 
-	public Child(String name, String surname, Human.Gender gender, Humans mother, Humans father){
+	public Child(String name, String surname, Human.Gender gender,
+	             Humans mother, Humans father){
 		super(name,surname, gender);
 		this.married = false;
 		this.patronimyc = (this.gender == "Male") ? name + "ovich" : name + "ovna";
-
+		this.mother = mother;
+		this.father = father;
 	}
 
 	@Override
 	public String toString() {
-		return "name = " + name + " " +
+		return  "Мать = "+mother.getName()+ " " + mother.getSurname() + "\n" +
+				"Отец = " + father.getName() + " " + father.getSurname() + "\n" +
+				"name = " + name + " " +
 				"surname= " + surname + " " +
 				"patronimyc = " + patronimyc + "\n" +
 				"gender = " + gender + " " +
--- Lesson1and2/DoChild.java
+++ Lesson1and2/DoChild.java
@@ -1,9 +1,6 @@
 package Lesson1and2;
 
-import java.util.ArrayList;
-import java.util.Map;
-
 public interface DoChild {
 
-	void DoChild(Humans parent2, Map<Integer, ArrayList<Humans>> genealogicTree);
+	void DoChild(Humans parent2, GenealogicTree<Child> genealogicTree);
 }
