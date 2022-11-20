87f2d03921e6e539b0d7bb2b3d5a06b242f2001e
Dmitriy <argizol@mail.ru>
2022-11-20 16:28:36 +0300

    Authority update

--- Lesson5and6/UserAuthority.java
+++ Lesson5and6/UserAuthority.java
@@ -4,24 +4,27 @@ import java.util.Map;
 
 public  class UserAuthority implements Authority{
 	private View view;
+	private UsersDB usersDB;
 
-	public UserAuthority(View view) {
-		this.view = view;
+	public UserAuthority(View view, UsersDB usersDB) {
+		this.view = view; this.usersDB = usersDB;
 	}
 
 	@Override
-	public boolean check(String login, String password, Map<String, String> usersDB) {
+	public boolean check() {
 
 		boolean bool = false;
+		String login = view.getLogin();
+		String password = view.getPassword();
 
-		if(!usersDB.containsKey(login)){
+		if(!usersDB.getUsersDB().containsKey(login)){
 			view.print("Пользователь не найден");
 			return false;
 		}
 
-		for(String user : usersDB.keySet()){
+		for(String user : usersDB.getUsersDB().keySet()){
 			if(user.equals(login)){
-				if(password.equals(usersDB.get(login))){
+				if(password.equals(usersDB.getUsersDB().get(login))){
 					view.print("Авторизация успешна");
 					bool =  true;
 				} else {
